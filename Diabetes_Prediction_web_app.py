76a8e38bacd83b0a6b7ebe44c2b325b1b64b630f
Houda2112 <132212546+Houda2112@users.noreply.github.com>
2023-05-09 23:35:12 +0100

    Update Diabetes_Prediction_web_app.py

--- Diabetes_Prediction_web_app.py
+++ Diabetes_Prediction_web_app.py
@@ -51,6 +51,47 @@ def main():
     Age = st.text_input('Age of the Person')
     
     
+    
+ # Convert inputs to numeric format
+pregnancies = int(pregnancies)
+glucose_level = int(glucose_level)
+blood_pressure = int(blood_pressure)
+skin_thickness = int(skin_thickness)
+insulin_level = int(insulin_level)
+bmi = float(bmi)
+diabetes_pedigree_function = float(diabetes_pedigree_function)
+age = int(age)
+
+# Determine risk of diabetes based on inputs
+if glucose_level > 140:
+    st.write('Your glucose level is high. You should consult with a doctor to determine if you are at risk for developing diabetes. In the meantime, try to limit your intake of sugary foods and drinks, and increase your physical activity.')
+elif bmi > 30:
+    st.write('Your BMI is high. You should consult with a doctor to determine if you are at risk for developing diabetes. In the meantime, try to incorporate more fruits and vegetables into your diet, and increase your physical activity.')
+elif age > 45:
+    st.write('Your age puts you at a higher risk for developing diabetes. You should consult with a doctor to determine if you are at risk. In the meantime, try to limit your intake of sugary foods and drinks, and increase your physical activity.')
+elif pregnancies > 0:
+    st.write('Your history of pregnancies puts you at a higher risk for developing diabetes. You should consult with a doctor to determine if you are at risk. In the meantime, try to incorporate more fruits and vegetables into your diet, and increase your physical activity.')
+else:
+    st.write('Based on your inputs, you appear to be at a lower risk for developing diabetes. However, it is always a good idea to maintain a healthy lifestyle to reduce your risk. Try to incorporate more fruits and vegetables into your diet, limit your intake of sugary foods and drinks, and increase your physical activity. It is also recommended that you schedule regular check-ups with your doctor to monitor your health.')    
+    
+# Determine risk of heart disease based on inputs
+if gender == 'Male' and age > 45:
+    if blood_pressure > 130 or glucose_level > 100 or bmi > 25:
+        st.write('You appear to be at risk for developing heart disease. It is important to maintain a healthy lifestyle by eating a balanced diet, exercising regularly, quitting smoking (if applicable), and reducing stress.')
+elif gender == 'Female' and age > 55:
+    if blood_pressure > 130 or glucose_level > 100 or bmi > 25:
+        st.write('You appear to be at risk for developing heart disease. It is important to maintain a healthy lifestyle by eating a balanced diet, exercising regularly, quitting smoking (if applicable), and reducing stress.')
+else:
+    st.write('Based on your inputs, you appear to be at a lower risk for developing heart disease. However, it is still important to maintain a healthy lifestyle to reduce your risk of developing heart disease.')
+    
+# Determine risk of Parkinson's disease based on inputs
+if age > 60 and insulin_level < 30:
+    st.write('Your insulin level is low, and you are above the age of 60. You should consult with a doctor to determine if you are at risk for developing Parkinson\'s disease.')
+else:
+    st.write('Based on your inputs, you appear to be at a lower risk for developing Parkinson\'s disease. However, it is always a good idea to maintain a healthy lifestyle to reduce your risk.')    
+    
+    
+    
     # code for Prediction
     diagnosis = ''
     
