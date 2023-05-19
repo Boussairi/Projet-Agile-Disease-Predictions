8411e6e3de5334429dfa9ab920b846ecf2f42663
Hamza Boussairi <103688769+Boussairi@users.noreply.github.com>
2023-05-19 21:35:59 +0100

    Update Diabetes_Prediction_web_app.py

--- Diabetes_Prediction_web_app.py
+++ Diabetes_Prediction_web_app.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open("C:/Users/Xps/Desktop/S4/Autoformation/Disease prediction/diabetes_model.sav", 'rb'))
+loaded_model = pickle.load(open("diabetes_model.sav", 'rb'))
 
 
 # creating a function for Prediction
