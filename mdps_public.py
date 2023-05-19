8a3fc93bb470ca171715353a13f4a569bb52f5ff
Hamza Boussairi <103688769+Boussairi@users.noreply.github.com>
2023-05-19 21:21:32 +0100

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -12,7 +12,7 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open(r"c:\users\xps\desktop\s4\autoformation\disease prediction\sprint2\diabetes_tuned_model.sav", 'rb'))
+diabetes_model = pickle.load(open(r"c:\users\xps\desktop\s4\autoformation\disease prediction\sprint2\diabetes_model.sav", 'rb'))
 
 heart_disease_model = pickle.load(open(r"c:\users\xps\desktop\s4\autoformation\disease prediction\sprint2\heart_disease_model.sav", 'rb'))
 
