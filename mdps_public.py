c246012900e6e3cd3813135c4c630756f09e0702
Hamza Boussairi <103688769+Boussairi@users.noreply.github.com>
2023-05-20 18:11:14 +0100

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -13,6 +13,11 @@ from streamlit_option_menu import option_menu
 # loading the saved models
 
 
+diabetes_model = pickle.load(open(r"diabetes_tuned_model.sav", 'rb'))
+
+heart_disease_model = pickle.load(open(r"heart_disease_model.sav", 'rb'))
+
+parkinsons_model = pickle.load(open(r"parkinsons_model.sav", 'rb'))
 
 
 # sidebar for navigation
