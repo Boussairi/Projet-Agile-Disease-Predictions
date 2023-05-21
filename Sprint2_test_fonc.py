a3ae3f26e963e592e04b1d140b9008c3ba591aa4
Hamza Boussairi <103688769+Boussairi@users.noreply.github.com>
2023-05-21 22:37:26 +0100

    Update Sprint2_test_fonc.py

--- Sprint2_test_fonc.py
+++ Sprint2_test_fonc.py
@@ -3,7 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-# Create a new Chrome browser instance
+from selenium.webdriver.chrome.service import Service
+
+
 browser = webdriver.Chrome(executable_path="chromedriver.exe")
 
 # Go to the given site
