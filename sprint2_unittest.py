7af8b4d8147307924a5790efcefb888da64e7fd8
Hamza Boussairi <103688769+Boussairi@users.noreply.github.com>
2023-05-21 16:33:19 +0100

    Update sprint2_unittest.py

--- sprint2_unittest.py
+++ sprint2_unittest.py
@@ -6,14 +6,6 @@ from mdps_public import *
 
 class TestDiseasePrediction(unittest.TestCase):
 
-with st.sidebar:
-    selected = option_menu('Multiple Disease Prediction System',                        
-                          ['Diabetes Prediction',
-                           'Heart Disease Prediction',
-                           'Parkinsons Prediction'],
-                          icons=['activity','heart','person'],
-                          default_index=0)
-    
     
 # Diabetes Prediction Page
 if (selected == 'Diabetes Prediction'):
