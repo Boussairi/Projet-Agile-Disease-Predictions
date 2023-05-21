b3ae3c736d7e2fbb9d7162a8991f9aa9d476af84
Hamza Boussairi <103688769+Boussairi@users.noreply.github.com>
2023-05-21 16:28:19 +0100

    Update sprint2_unittest.py

--- sprint2_unittest.py
+++ sprint2_unittest.py
@@ -6,19 +6,35 @@ from mdps_public import *
 
 class TestDiseasePrediction(unittest.TestCase):
 
+with st.sidebar:
+    
+    selected = option_menu('Multiple Disease Prediction System',
+                          
+                          ['Diabetes Prediction',
+                           'Heart Disease Prediction',
+                           'Parkinsons Prediction'],
+                          icons=['activity','heart','person'],
+                          default_index=0)
+    
+    
+# Diabetes Prediction Page
+if (selected == 'Diabetes Prediction'):
     def test_diabetes_prediction(self):
-        input_values = ['1', '85', '66', '29', '0', '26.6', '0.351', '31']
+        input_values = [1, 85, 66, 29, 0, 26.6, 0.351, 31]
         expected_output = 'The person is not diabetic'
-        with patch('builtins.input', side_effect=input_values), patch('sys.stdout', new=StringIO()) as fake_out:
-            diabetes_prediction()
+       # with patch('builtins.input', side_effect=input_values), patch('sys.stdout', new=StringIO()) as fake_out:
+        #    diabetes_prediction()
             self.assertEqual(fake_out.getvalue().strip(), expected_output)
-
-    def test_heart_disease_prediction(self):
-        input_values = ['20', '1', '0', '5', '21', '0', '0', '6', '1', '0.9', '1', '2', '2']
-        expected_output = 'The person does not have any heart disease'
-        with patch('builtins.input', side_effect=input_values), patch('sys.stdout', new=StringIO()) as fake_out:
-            heart_disease_prediction()
-            self.assertEqual(fake_out.getvalue().strip(), expected_output)
+        result = diabetes_model.predict([input_values])                          
+        self.assertEqual(result, expected_output)
+            
+#if (selected == 'Heart Disease Prediction'):
+ #   def test_heart_disease_prediction(self):
+  #      input_values = [20, 1, 0, 5, 21, 0, 0, 6, 1, 0.9, 1, 2, 2]
+   #     expected_output = 'The person does not have any heart disease'
+    #    with patch('builtins.input', side_effect=input_values), patch('sys.stdout', new=StringIO()) as fake_out:
+     #       heart_disease_prediction()
+      #      self.assertEqual(fake_out.getvalue().strip(), expected_output)
 
 #    def test_parkinsons_prediction(self):
  #       input_values = ['150', '200', '100', '0.5', '0.02', '0.05', '0.08', '0.06', '0.04', '0.06', '0.04', '0.07', '0.05', '0.06', '0.05', '0.02', '0.08', '0.07', '0.04', '0.05', '0.08', '0.06']
