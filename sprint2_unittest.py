7ea8fd73fd5f41f1848be9cc717b1fa445ad024a
Hamza Boussairi <103688769+Boussairi@users.noreply.github.com>
2023-05-21 15:34:08 +0100

    Update sprint2_unittest.py

--- sprint2_unittest.py
+++ sprint2_unittest.py
@@ -3,28 +3,30 @@ from unittest.mock import patch
 from io import StringIO
 from mdps_public import *
 
+
 class TestDiseasePrediction(unittest.TestCase):
 
     def test_diabetes_prediction(self):
-        input_values = ['1','85','66','29','0','26.6','0.351','31']
-        output = StringIO()
-        with patch('builtins.input', side_effect=input_values), patch('sys.stdout', output):
+        input_values = ['1', '85', '66', '29', '0', '26.6', '0.351', '31']
+        expected_output = 'The person is not diabetic'
+        with patch('builtins.input', side_effect=input_values), patch('sys.stdout', new=StringIO()) as fake_out:
             diabetes_prediction()
-        self.assertEqual(output.getvalue().strip(), 'The person is not diabetic')
+            self.assertEqual(fake_out.getvalue().strip(), expected_output)
 
     def test_heart_disease_prediction(self):
-        input_values = ['20','1',0,'5','21','0','0','6','1','0.9','1','2','2']
-        output = StringIO()
-        with patch('builtins.input', side_effect=input_values), patch('sys.stdout', output):
+        input_values = ['20', '1', '0', '5', '21', '0', '0', '6', '1', '0.9', '1', '2', '2']
+        expected_output = 'The person does not have any heart disease'
+        with patch('builtins.input', side_effect=input_values), patch('sys.stdout', new=StringIO()) as fake_out:
             heart_disease_prediction()
-        self.assertEqual(output.getvalue().strip(), 'The person does not have any heart disease')
+            self.assertEqual(fake_out.getvalue().strip(), expected_output)
 
- #   def test_parkinsons_prediction(self):
-  #      input_values = ['150', '200', '100', '0.5', '0.02', '0.05', '0.08', '0.06', '0.04', '0.06', '0.04', '0.07', '0.05', '0.06', '0.05', '0.02', '0.08', '0.07', '0.04', '0.05', '0.08', '0.06']
-   #     output = StringIO()
-    #    with patch('builtins.input', side_effect=input_values), patch('sys.stdout', output):
-     #       parkinsons_prediction()
-      #  self.assertEqual(output.getvalue().strip(), "The person doesn't have Parkinson's disease")
+#    def test_parkinsons_prediction(self):
+ #       input_values = ['150', '200', '100', '0.5', '0.02', '0.05', '0.08', '0.06', '0.04', '0.06', '0.04', '0.07', '0.05', '0.06', '0.05', '0.02', '0.08', '0.07', '0.04', '0.05', '0.08', '0.06']
+  #      expected_output = "The person doesn't have Parkinson's disease"
+   #     with patch('builtins.input', side_effect=input_values), patch('sys.stdout', new=StringIO()) as fake_out:
+    #        parkinsons_prediction()
+     #       self.assertEqual(fake_out.getvalue().strip(), expected_output)
+
 
 if __name__ == '__main__':
     unittest.main()
